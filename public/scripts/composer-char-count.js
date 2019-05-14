a25e23c16730fcf476937ce4d166907c93096152
Thomas Boyer <thomas.james.boyer@gmail.com>
2019-05-14 22:35:52 +0000

    Refactored to refer to counter through traversal instead of through class name

--- public/scripts/composer-char-count.js
+++ public/scripts/composer-char-count.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
   const maxValue = $('.counter').text();
   $('#tweet-input').on('input', function()
     {
-      $('.counter').text(maxValue - $(this).val().length);
+      $(this).next().children().last().text(maxValue - $(this).val().length);
 
-      $('.counter').text() < 0 ? $('.counter').addClass("invalid") : $('.counter').removeClass("invalid");
+      $(this).next().children().last().text() < 0 ? $('.counter').addClass("invalid") : $('.counter').removeClass("invalid");
     })
 });
